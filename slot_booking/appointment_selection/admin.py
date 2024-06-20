6951015c7535e511e1687c8124a0bd7b1b3c045f
Marul Bhardwaj <158758744+MB-077@users.noreply.github.com>
2024-06-20 13:33:33 +0530

    updated database logic

--- slot_booking/appointment_selection/admin.py
+++ slot_booking/appointment_selection/admin.py
@@ -4,12 +4,16 @@ from django.contrib import admin
 
 from .models import *
 
-class TimeSlotAdmin(admin.ModelAdmin):
-    list_display = ('start_time', 'end_time', 'is_booked', 'booked_by')
-    list_filter = ('is_booked',)
-    # prepopulated_fields = {'is_booked':('booked_by',)}
 
+class TimeSlotAdmin(admin.ModelAdmin):
+    list_display = ('start_time', 'end_time', 'is_booked', )
+    list_filter = ('is_booked', )
+
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('doctor', 'patient', 'date', 'time_slot', 'reschedule_requested', 'is_approved', )
+    list_filter = ('doctor', 'date', 'reschedule_requested', 'is_approved', )
+    
 admin.site.register(Doctor)
 admin.site.register(Patient)
 admin.site.register(TimeSlot, TimeSlotAdmin)
-admin.site.register(Appointment)
+admin.site.register(Appointment, AppointmentAdmin)
