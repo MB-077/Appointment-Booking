7a9a5ae3fa7ef75283d910a741d733c78a1992f4
KRRISH PUNJ <142911896+Ovocode05@users.noreply.github.com>
2024-07-21 01:21:10 +0530

    some setup profile

--- Frontend/src/Pages/Profile.jsx
+++ Frontend/src/Pages/Profile.jsx
@@ -3,14 +3,21 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Button from "./../Components/Button";
 import axios from "axios";
+import cat from "./../images/catto.jpg";
 // export function profileLoader({ request }) {
 //   return newURL(request.url).searchParams.get("message");
 // }
 
 const PatientProfile = ({ userId }) => {
-  const [profileData, setProfileData] = React.useState(null);
+  //getting the user Id
+  const userME = localStorage.getItem("userData");
+  const token = localStorage.getItem("token");
+  const info = JSON.parse(userME);
+  const id_main = info.patient_id;
+
+  const [profileData, setProfileData] = React.useState([]);
   const [formData, setFormData] = React.useState({
-    patient: "",
+    patient_id: `${id_main}`,
     age: "",
     gender: "",
     address: "",
@@ -19,54 +26,54 @@ const PatientProfile = ({ userId }) => {
   });
   const [isFormSubmitted, setIsFormSubmitted] = React.useState(false);
 
-  //getting the user Id
-  const user = localStorage.getItem("userData");
-  const token = localStorage.getItem("token");
-  const info = JSON.parse(user);
-  const id_main = info.patient_id;
-
   React.useEffect(() => {
-    const fetchData = async (userId) => {
-      if (isFormSubmitted) {
-        try {
-          const response = await axios.get(
-            `http://127.0.0.1:8000/patients-detail/${userId}/`,
-            {
-              headers: {
-                Authorization: `token ${token}`,
-              },
-            }
-          );
-          setProfileData(response.data);
-        } catch (error) {
-          console.error(error);
-        }
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `http://127.0.0.1:8000/patients-detail/`,
+          {
+            headers: {
+              Authorization: `token ${token}`,
+            },
+          }
+        );
+        const data = response.data;
+        console.log(data);
+        setProfileData(data);
+      } catch (error) {
+        console.error(error);
       }
     };
 
-    fetchData(id_main);
+    fetchData();
 
-    const interval = setInterval(() => {
-      if (!isFormSubmitted) {
-        toast.info("Please fill out your profile form.", { autoClose: false });
-      }
-    }, 300000);
+    if (profileData.length === 0) {
+      const interval = setInterval(() => {
+        if (profileData.length === 0) {
+          toast.info("Please fill out your profile form.", {
+            autoClose: false,
+          });
+        }
+      }, 300000);
 
-    return () => clearInterval(interval);
-  }, [isFormSubmitted, userId]);
+      return () => clearInterval(interval);
+    }
+  }, [token]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+    if (name !== "patient_id") {
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+    }
   };
 
-  const postData = async (userId) => {
+  const postData = async () => {
     try {
-      await axios.post(
-        `http://127.0.0.1:8000/patients-detail/${userId}/`,
+      const response = await axios.post(
+        `http://127.0.0.1:8000/patients-detail/`,
         formData,
         {
           headers: {
@@ -74,6 +81,7 @@ const PatientProfile = ({ userId }) => {
           },
         }
       );
+      console.log(response.data);
       setIsFormSubmitted(true);
       toast.dismiss();
     } catch (error) {
@@ -83,32 +91,19 @@ const PatientProfile = ({ userId }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(id_main);
-    console.log(formData);
-    postData(id_main);
+    postData();
   };
 
+  const [el] = profileData;
   return (
     <div className="p-6  h-[80vh]  rounded-lg text-white">
-      {!isFormSubmitted ? (
+      {profileData.length === 0 ? (
         <div>
           <h2 className="text-2xl text-center mb-8 ">
             Fill in the details to book a slots
           </h2>
           <form onSubmit={handleSubmit} className=" h-[64.6vh]">
             <div className="grid grid-cols-3  h-full  m-5">
-              <div className="flex flex-col ">
-                <p className="text-lg font-semibold">Name</p>
-                <input
-                  type="text"
-                  name="patient"
-                  placeholder=" Your name here"
-                  className=" px-3 py-2 w-[300px] rounded-[3px] text-[17px] bg-transparent border-[2px] border-white outline-none"
-                  autoComplete="off"
-                  onChange={handleChange}
-                />
-              </div>
-
               <div className="flex flex-col gap-1">
                 <p className="text-lg font-semibold">Age</p>
                 <input
@@ -118,6 +113,7 @@ const PatientProfile = ({ userId }) => {
                   className=" px-3 py-2 w-[300px] rounded-[3px] text-[17px] bg-transparent border-[2px] border-white outline-none"
                   autoComplete="off"
                   onChange={handleChange}
+                  value={formData.age}
                 />
               </div>
 
@@ -130,6 +126,7 @@ const PatientProfile = ({ userId }) => {
                   className=" px-3 py-2 w-[300px] rounded-[3px] text-[17px] bg-transparent border-[2px] border-white outline-none"
                   onChange={handleChange}
                   autoComplete="off"
+                  value={formData.gender}
                 />
               </div>
 
@@ -143,6 +140,7 @@ const PatientProfile = ({ userId }) => {
                   className=" px-3 py-2 w-[300px] rounded-[3px] text-[17px] bg-transparent border-[2px] border-white outline-none"
                   autoComplete="off"
                   onChange={handleChange}
+                  value={formData.address}
                 />
               </div>
 
@@ -155,6 +153,7 @@ const PatientProfile = ({ userId }) => {
                   className=" px-3 py-2 w-[300px] rounded-[3px] text-[17px] bg-transparent border-[2px] border-white outline-none"
                   onChange={handleChange}
                   autoComplete="off"
+                  value={formData.blood_group}
                 />
               </div>
 
@@ -167,6 +166,7 @@ const PatientProfile = ({ userId }) => {
                   className=" px-3 py-2 w-[300px] rounded-[3px] text-[17px] bg-transparent border-[2px] border-white outline-none"
                   autoComplete="off"
                   onChange={handleChange}
+                  value={formData.zip_code}
                 />
               </div>
             </div>
@@ -180,17 +180,99 @@ const PatientProfile = ({ userId }) => {
           </form>
         </div>
       ) : (
-        <div>
-          <h2 className="text-2xl font-semibold">Profile Information</h2>
-          <p className="mt-4">
-            <strong>Name:</strong> {profileData?.name}
-          </p>
-          <p className="mt-4">
-            <strong>Email:</strong> {profileData?.email}
-          </p>
-          <p className="mt-4">
-            <strong>Age:</strong> {profileData?.age}
-          </p>
+        <div className="w-full  bg-gradient text-white p-6 font-semibold rounded-lg shadow-md h-[76vh]">
+          <div className="absolute left-[22rem]">
+            <div className="flex items-center space-x-6 mb-[26px] ">
+              <img
+                className="w-28 h-28 rounded-full object-cover"
+                src={cat}
+                alt="Patient"
+              />
+              <div>
+                <h2 className="text-4xl tracking-wider font-bold ">
+                  {el.patient.charAt(0).toUpperCase() + el.patient.slice(1)}
+                </h2>
+              </div>
+            </div>
+
+            <div className="w-[1230px] bg-n-11 top-[130px] rounded-b-md absolute h-[56vh] -left-[54px]">
+              {" "}
+            </div>
+            <div className="space-y-7 grid grid-cols-3 gap-5 ">
+              <div className="relative top-3">
+                <h3 className="text-lg font-semibold text-gray-500 relative">
+                  Personal Information
+                </h3>
+
+                <div>
+                  {/* <p className="bg-transparent text-white px-2 py-1 w-[75%] h-full m-1 list-none rounded-sm">
+                    <span className="font-semibold text-lg  ">Phone:</span>{" "}
+                    {el.phone}
+                  </p> */}
+                  <p className="bg-transparent text-white px-2 py-1 w-[75%] h-full m-1 list-none rounded-sm">
+                    <span className="font-semibold text-lg">Age:</span> {el.age}
+                  </p>
+                  <p className="bg-transparent text-white px-2 py-1 w-[75%] h-full m-1 list-none rounded-sm">
+                    <span className="font-semibold text-lg">Blood Group:</span>{" "}
+                    {el.bloodGroup}
+                  </p>
+                  <p className="bg-transparent text-white px-2 py-1 w-[75%] h-full m-1 list-none rounded-sm">
+                    <span className="font-semibold text-lg">Gender:</span>{" "}
+                    {el.gender}
+                  </p>
+                  <p className="bg-transparent text-white px-2 py-1 w-[75%] h-full m-1 list-none rounded-sm">
+                    <span className="font-semibold text-lg">ZipCode: </span>{" "}
+                    {el.zip_code}
+                  </p>
+                  <p className="bg-transparent text-white px-2 py-1 w-[75%] h-full m-1 list-none rounded-sm">
+                    <span className="font-semibold text-lg">Mobile: </span>{" "}
+                    {info.phone_number}
+                  </p>
+                </div>
+              </div>
+
+              <div className="relative -top-4">
+                <h3 className="text-lg font-semibold text-gray-500">Address</h3>
+                <p className="bg-transparent text-white px-2 py-1 w-[75%] h-[60%] m-1 list-none rounded-sm">
+                  {el.address}
+                </p>
+              </div>
+
+              <div className="relative -top-4">
+                <h3 className="text-lg font-semibold text-gray-500">
+                  Last Appointments
+                </h3>
+                <p>No Appointments till now</p>
+              </div>
+
+              <div className="relative">
+                <h3 className="text-lg font-semibold text-gray-500">
+                  Emergency Contact
+                </h3>
+                <p className="bg-transparent text-white px-2 py-1 w-[75%] m-1 list-none rounded-sm">
+                  {el.emergencyContact}
+                </p>
+              </div>
+
+              <div className="relative">
+                <h3 className="text-lg font-semibold text-gray-500">
+                  Insurance
+                </h3>
+                <p className="bg-transparent text-white px-2 py-1 w-[75%] m-1 list-none rounded-sm">
+                  No details
+                </p>
+              </div>
+
+              <div className="mt-6 relative h-[50px] -top-2 flex gap-10">
+                <button className="bg-blue-500 text-white px-4  rounded-md">
+                  Change Password
+                </button>
+                <button className="bg-blue-500 text-white px-4 rounded-md">
+                  Edit Profile
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       )}
       <ToastContainer />
@@ -199,105 +281,3 @@ const PatientProfile = ({ userId }) => {
 };
 
 export default PatientProfile;
-{
-  /* <div className="w-full  bg-gradient text-white p-6 font-semibold rounded-lg shadow-md h-[80vh]">
-      <div className="absolute left-[22rem]">
-        <div className="flex items-center space-x-6 mb-[26px] ">
-          <img
-            className="w-28 h-28 rounded-full object-cover"
-            src={Test1}
-            alt="Patient"
-          />
-          <div>
-            <h2 className="text-2xl font-bold ">{patientData.name}</h2>
-            <p className="text-gray-400">{patientData.occupation}</p>
-          </div>
-        </div>
-
-        <div className="w-[1230px] bg-n-11 top-[130px] rounded-b-md absolute h-[60vh] -left-[54px]">
-          {" "}
-        </div>
-        <div className="space-y-7 grid grid-cols-3 gap-5 ">
-          <div className="relative top-3">
-            <h3 className="text-lg font-semibold text-gray-500 relative">
-              Personal Information
-            </h3>
-
-            <div>
-              <p className="bg-transparent text-white px-2 py-1 w-[75%] h-full m-1 list-none rounded-sm">
-                <span className="font-semibold text-lg  ">Phone:</span>{" "}
-                {patientData.phone}
-              </p>
-              <p className="bg-transparent text-white px-2 py-1 w-[75%] h-full m-1 list-none rounded-sm">
-                <span className="font-semibold text-lg">Age:</span>{" "}
-                {patientData.age}
-              </p>
-              <p className="bg-transparent text-white px-2 py-1 w-[75%] h-full m-1 list-none rounded-sm">
-                <span className="font-semibold text-lg">Blood Group:</span>{" "}
-                {patientData.bloodGroup}
-              </p>
-              <p className="bg-transparent text-white px-2 py-1 w-[75%] h-full m-1 list-none rounded-sm">
-                <span className="font-semibold text-lg">Gender:</span>{" "}
-                {patientData.gender}
-              </p>
-            </div>
-          </div>
-
-          <div className="relative -top-4">
-            <h3 className="text-lg font-semibold text-gray-500">Address</h3>
-            <p className="bg-transparent text-white px-2 py-1 w-[75%] h-[60%] m-1 list-none rounded-sm">
-              {patientData.address}
-            </p>
-          </div>
-
-          <div className="relative -top-4">
-            <h3 className="text-lg font-semibold text-gray-500">
-              Last Appointments
-            </h3>
-            <ul>
-              {patientData.lastAppointments.map((appointment, index) => (
-                <li
-                  key={index}
-                  className="bg-transparent text-white px-2 py-1 w-[75%] m-1 list-none rounded-sm"
-                >
-                  {appointment}
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          <div className="relative">
-            <h3 className="text-lg font-semibold text-gray-500">Diagnostics</h3>
-            <p className="bg-transparent text-white px-2 py-1 w-[75%] m-1 list-none rounded-sm">
-              {patientData.diagnostics}
-            </p>
-          </div>
-
-          <div className="relative">
-            <h3 className="text-lg font-semibold text-gray-500">
-              Emergency Contact
-            </h3>
-            <p className="bg-transparent text-white px-2 py-1 w-[75%] m-1 list-none rounded-sm">
-              {patientData.emergencyContact}
-            </p>
-          </div>
-
-          <div className="relative">
-            <h3 className="text-lg font-semibold text-gray-500">Insurance</h3>
-            <p className="bg-transparent text-white px-2 py-1 w-[75%] m-1 list-none rounded-sm">
-              {patientData.insurance}
-            </p>
-          </div>
-
-          <div className="mt-6 relative -top-2 flex gap-5">
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
-              Change Password
-            </button>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
-              Edit Profile
-            </button>
-          </div>
-        </div>
-      </div>
-    </div> */
-}
